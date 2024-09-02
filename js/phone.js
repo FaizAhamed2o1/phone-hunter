1d2c05a34f8b73048e525d93436091df91895040
FaizAhamed2o1 <faizahmed23014@gmail.com>
2024-09-02 10:27:59 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,9 @@
+const loadPhone = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await response.json();
+  const phones = data.data;
+  console.log(phones);
+};
+loadPhone();
